9c2578a91d2a06f08bc6aba249f90435c02f806e
Teo2602 <167240738+Teo2602@users.noreply.github.com>
2025-04-08 10:23:52 -0300

    creacion del tablero

--- NavalWar.py
+++ NavalWar.py
@@ -1,13 +1,12 @@
 #creacion de tablero
-
-
-
-
-
-
-
-
-
+tablero : list =[];
+tamaño : int=int(input("Cual va a ser el ancho y largo del tablero?"));
+for fila in range(tamaño):
+    columna:list=[]
+    for i in range(tamaño):
+        columna.append(False)
+    tablero.append(columna)
+    print(tablero[fila])
 
 
 
